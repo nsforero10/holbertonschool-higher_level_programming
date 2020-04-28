bbc068c6d76d2af0b71fec82311c3b38c3ea4416
Nicolas Forero <ns.forero10@gmail.com>
2020-04-28 18:29:13 -0500

    [Feature] Adding 12 task 0x01 project

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def fizzbuzz():
     for i in range(1, 101):
-        if (i % 3) == 0  and (i % 5) == 0:
+        if (i % 3) == 0 and (i % 5) == 0:
             print('FizzBuzz', end=' ')
         elif (i % 3) == 0:
             print('Fizz', end=' ')
@@ -10,4 +10,3 @@ def fizzbuzz():
         else:
             print(i, end=' ')
     print()
-    
