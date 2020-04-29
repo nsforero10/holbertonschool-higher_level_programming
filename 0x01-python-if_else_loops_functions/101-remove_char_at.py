5c052c42fc3f197a7a28a49c4488dfc157f621e3
Nicolas Forero <ns.forero10@gmail.com>
2020-04-28 20:55:58 -0500

    [Feature] Adding 101 task 0x01 project

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
     cp = ''
-    for i in range(0, lent(str)):
+    for i in range(0, len(str)):
         if i != n:
             cp += str[i]
     return cp
