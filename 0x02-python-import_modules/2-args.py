e68589ce55c755e4cfa76997dfb2c55cf15e6eae
Nicolas Forero <ns.forero10@gmail.com>
2020-04-30 13:15:28 -0500

    [Fix] Fixing 1 case 2 task 0x02

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,9 +3,14 @@ import sys
 if __name__ == '__main__':
 
     length = len(sys.argv) - 1
-    print('{} arguments:'.format(length)) if length > 1 \
-        else print('{} arguments.'.format(length))
+    if length == 0:
+        print('{} arguments.'.format(length))
+    else:
 
-    if length != 0:
+        if length == 1:
+            print('{} argument:'.format(length))
+        else:
+            print('{} arguments:'.format(length))
+
         for i in range(1, length + 1):
             print('{:d}: {}'.format(i, sys.argv[i]))
