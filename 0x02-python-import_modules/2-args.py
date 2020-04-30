e09ebb9be5fc0db722e16e0be3d822f297904bc7
Nicolas Forero <ns.forero10@gmail.com>
2020-04-30 02:34:56 -0500

    [Feature] Adding 2 task 0x02

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+import sys
+
+length = len(sys.argv)
+print('{} arguments:'.format(length - 1)) if length - 1 > 0 \
+    else print('{} arguments.'.format(length - 1))
+
+if length - 1 != 0:
+    for i in range(1, length):
+        print('{:d}: {}'.format(i, sys.argv[i]))
