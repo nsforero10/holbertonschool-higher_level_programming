0dc6b62db19f0762b51462c164b28b4216515342
Nicolas Forero <ns.forero10@gmail.com>
2020-04-30 03:46:42 -0500

    [Feature] Adding 100 task 0x02

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+import sys
+
+av = sys.argv
+for v in av:
+    print(v)
+if len(av) != 4:
+    print('Usage: ./100-my_calculator.py <a> <operator> <b>')
+    exit(1)
+elif av[2] not in '+-*/':
+    print("Unknown operator. Available operators: +, -, * and /")
+else:
+    switcher = {
+        '+': add,
+        '-': sub,
+        '*': mul,
+        '/': div,
+    }
+    print(switcher.get(av[2])(int(av[1]), int(av[3])))
