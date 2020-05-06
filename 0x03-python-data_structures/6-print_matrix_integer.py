92619c152c2011a204de6198e8fe46fe24a7b79c
Nico F <ns.forero10@gmail.com>
2020-05-06 04:45:48 +0000

    [Feature] Adding 0x03 6 task

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_matrix_integer(m=[[]]):
     print('\n'.join(
-        [' '.join(['{:d}'.format(idx)for i in r])for r in m]
+        [' '.join(['{:d}'.format(i)for i in r])for r in m]
     ))
