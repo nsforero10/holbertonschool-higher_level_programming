8b00d86c4740f4219f0260c53e948875f57db49d
Nico F <ns.forero10@gmail.com>
2020-05-06 04:49:16 +0000

    [Feature] Adding 0x03 7 task

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -3,11 +3,11 @@ def add_tuple(tp_a=(), tp_b=()):
 
     if len(tp_a) is 0:
         tp_a = (0, 0)
-    elif len(tuple_a) is 1:
+    elif len(t[_a) is 1:
         tp_a = (tp_a[0], 0)
     if len(tp_b) is 0:
         tp_b = (0, 0)
     elif len(tp_b) is 1:
-        tp_b = (tuple_b[0], 0)
+        tp_b = (tp_b[0], 0)
 
     return (tp_a[0] + tp_b[0], tp_a[1] + tp_b[1])
