401f2bcc4a8dcfcf29bd0938c4747adceb37db31
Nicolas Forero <ns.forero10@gmail.com>
2020-05-07 15:33:22 -0500

    [Feautre] Adding 4 task 0x04

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
+    diff = []
+    for el1 in set_1:
+        if el1 not in set_2:
+            diff.append(el1)
+    for el2 in set_2:
+        if el2 not in set_1:
+            diff.append(el2)
+    return diff
