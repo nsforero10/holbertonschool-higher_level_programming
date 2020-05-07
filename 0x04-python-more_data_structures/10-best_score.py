5bafb2c607504203f08da018e1ba62336515200e
Nicolas Forero <ns.forero10@gmail.com>
2020-05-07 16:34:20 -0500

    [Feautre] Adding 10 task 0x04

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
+    if a_dictionary:
+        best = (0, 0)
+        for key, value in a_dictionary.items():
+            if best[1] < value:
+                best = (key, value)
+        return best[0]
