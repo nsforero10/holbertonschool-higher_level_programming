2faaa5f7f246bf83487734950161dcc904ba6777
Nicolas Forero <ns.forero10@gmail.com>
2020-05-07 15:20:05 -0500

    [Feautre] Adding 3 task 0x04

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
+    commons = []
+    for el1 in set_1:
+        if el1 in set_2:
+            commons.append(el1)
+            break
+    return commons
