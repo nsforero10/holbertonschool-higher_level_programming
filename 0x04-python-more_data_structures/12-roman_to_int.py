422de04f8dea791e72f0962e7372ec76ef0265e0
Nicolas Forero <ns.forero10@gmail.com>
2020-05-07 20:37:56 -0500

    [Feautre] Adding 12 task 0x04

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,2 +1,24 @@
 #!/usr/bin/python3
-def weight_average(my_list=[]):
+def roman_to_int(roman_string):
+    if (roman_string is None) or (type(roman_string) is not str):
+        return 0
+
+    rom = 0
+    nums = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+
+    for ch in range(len(roman_string)):
+        if ch == len(roman_string) - 1:
+            rom += nums[roman_string[ch]]
+        elif nums[roman_string[ch]] >= nums[roman_string[ch + 1]]:
+            rom += nums[roman_string[ch]]
+        else:
+            rom -= nums[roman_string[ch]]
+    return rom
