61990e4b358cdc989d7b20da4a8390b137f2b8c2
Nicolas Forero <ns.forero10@gmail.com>
2020-05-07 20:41:42 -0500

    [Feautre] Adding 100 task 0x04

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python3
-def square_matrix_map(matrix=[]):
+def weight_average(my_list=[]):
+    if my_list:
+        sc = 0
+        weight = 0
+        for elem in my_list:
+            sc += elem[0] * elem[1]
+            weight += elem[1]
+        return sc / weight
+    return 0
