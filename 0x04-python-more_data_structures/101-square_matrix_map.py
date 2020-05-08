a2fb383bebab1697a37f5a7d6581dc378e47a599
Nicolas Forero <ns.forero10@gmail.com>
2020-05-07 20:45:09 -0500

    [Feautre] Adding 101 task 0x04

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    return list(map(lambda row: list(map(lambda elem: elem ** 2, row)), matrix))
+    return list(map(lambda row: list(map(lambda el: el ** 2, row)), matrix))
