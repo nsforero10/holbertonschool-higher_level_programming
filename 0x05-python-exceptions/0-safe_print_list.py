7b5cf6bf965a42681ad3220edcb75e433e25ab1b
Nicolas Forero <ns.forero10@gmail.com>
2020-05-18 18:58:51 -0500

    [Feature] Adding 0 task 0x05

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,7 +2,7 @@
 def safe_print_list(my_list=[], x=0):
     try:
         for i in range(x):
-            print('{}'.format(my_list[i]), end=" ")
+            print('{}'.format(my_list[i]), end='')
         print()
         return x
     except IndexError:
