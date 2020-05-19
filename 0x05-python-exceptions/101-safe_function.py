71a6fd420230d83a7ac5cc4628019a7e7c286829
Nicolas Forero <ns.forero10@gmail.com>
2020-05-18 19:30:38 -0500

    [Feature] Adding 8 tasks 0x05

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,10 @@
+from sys import stderr
+
+
+def safe_function(fct, *args):
+    try:
+        r = fct(*args)
+    except Exception as err:
+        r = None
+        print("Exception:", err, file=stderr)
+    return r
