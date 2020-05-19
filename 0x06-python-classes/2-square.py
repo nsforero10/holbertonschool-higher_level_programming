89f68e10bc31d0f2aa4081f8cdde164f0ea41579
Nicolas Forero <ns.forero10@gmail.com>
2020-05-19 16:23:45 -0500

    [Feature] Adding 2 tasks 0x06

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""Square Class"""
+
+
+class Square:
+    """Empty Square class"""
+    def __init__(self, size):
+        if size is not int:
+            TypeError('size must be a integer')
+        if size < 0:
+            raise ValueError('size must be >=0')
+        self.__size = size
