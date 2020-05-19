b2e0673cc3ee54875201b8d1f3e644dbac85a7ab
Nicolas Forero <ns.forero10@gmail.com>
2020-05-18 19:21:23 -0500

    [Feature] Adding 4 task 0x05

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,2 +1,18 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
+    r = []
+    for i in range(list_length):
+        try:
+            v = my_list_1[i] / my_list_2[i]
+        except ZeroDivisionError:
+            print('division by 0')
+            v = 0
+        except TypeError:
+            print('wrong type')
+            v = 0
+        except IndexError:
+            print('out of range')
+            v = 0
+        finally:
+            r.append(v)
+    return r
