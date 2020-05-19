490894b3cbced76badf3cb3d5aeb6414993c6271
Nicolas Forero <ns.forero10@gmail.com>
2020-05-19 17:30:38 -0500

    [Feature] Adding 6 tasks 0x06

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -35,7 +35,7 @@ class Square:
 
     def my_print(self):
         if self.size != 0:
-            if self.position[1] != 0
+            if self.position[1] != 0:
                 print('\n' * self.position[1], end='')
             for i in range(self.size):
                 print(' ' * self.position[0], end='')
