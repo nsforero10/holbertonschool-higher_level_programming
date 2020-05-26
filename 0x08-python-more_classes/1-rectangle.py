3e0e95df208670a3950b766384be139059f85ad8
Nicolas Forero <ns.forero10@gmail.com>
2020-05-25 20:32:51 -0500

    [Feature] Adding 1 task 0x08

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -4,7 +4,7 @@
 
 class Rectangle:
     """Rectangle class"""
-    
+
     def __init__(self, width=0, height=0):
         self.height = height
         self.width = width
@@ -28,7 +28,7 @@ class Rectangle:
     @width.setter
     def width(self, value):
         if not isinstance(value, int):
-            raise TypeError('width must be a in integer')
+            raise TypeError("width must be an integer")
         if value < 0:
-            raise ValueError('width must be >=0')
+            raise ValueError("width must be >= 0")
         self.__width = value
