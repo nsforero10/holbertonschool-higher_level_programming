0630e1addae080556873c3ce9837ea73c9db8089
Nicolas Forero <ns.forero10@gmail.com>
2020-05-25 20:47:05 -0500

    [Feature] Adding 2 task 0x08

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -37,4 +37,6 @@ class Rectangle:
         return self.__width * self.__height
 
     def perimeter(self):
+        if self.height == 0 or self.width == 0:
+            return 0
         return self.__width * 2 + self.__height * 2
