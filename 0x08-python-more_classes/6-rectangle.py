d3f61b2721a2963f6fc81016daed242372fa2447
Nicolas Forero <ns.forero10@gmail.com>
2020-05-25 23:44:09 -0500

    [Feature] Adding 6 task 0x08

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -16,10 +16,6 @@ class Rectangle:
     def height(self):
         return self.__height
 
-    @property
-    def width(self):
-        return self.__width
-
     @height.setter
     def height(self, value):
         if not isinstance(value, int):
@@ -27,6 +23,10 @@ class Rectangle:
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
+
+    @property
+    def width(self):
+        return self.__width
 
     @width.setter
     def width(self, value):
@@ -40,9 +40,9 @@ class Rectangle:
         return self.__width * self.__height
 
     def perimeter(self):
-        if self.height == 0 or self.width == 0:
+        if self.__width == 0 or self.__height == 0:
             return 0
-        return self.__width * 2 + self.__height * 2
+        return 2 * (self.__width + self.__height)
 
     def __str__(self):
         str_rep = ''
@@ -55,8 +55,8 @@ class Rectangle:
         return str_rep
 
     def __repr__(self):
-        return 'Rectangle({}, {})'.format(self.width, self.height)
+        return "Rectangle({:d}, {:d})".format(self.__width, self.__height)
 
     def __del__(self):
         Rectangle.number_of_instances -= 1
-        print('Bye rectangle...')
+        print("Bye rectangle...")
