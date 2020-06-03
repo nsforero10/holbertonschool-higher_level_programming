b1a921d46a8b654e342306f2d3bb01d374f32c2f
Nicolas Forero <ns.forero10@gmail.com>
2020-06-02 23:58:42 -0500

    [Fix] Fixing typos task cases 0x0A

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,12 +1,16 @@
-# !/usr/bin/python3
+#!/usr/bin/python3
 """
-100-my-int.py
+100-my_int.py
 """
 
 
-def add_attribute(obj, name, value):
-    """ Adds a attribute to a given object """
-    if hasattr(obj, '__dict__') is False:
-        raise TypeError("can't add new attribute")
-    else:
-        setattr(obj, name, value)
+class MyInt(int):
+    """ Class that inherits form int """
+    def __init__(self, value):
+        self.value = value
+    """ change the value"""
+    def __eq__(self, other):
+        return self.value != other
+    """ change te value"""
+    def __ne__(self, other):
+        return self.value == other
--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,1 +1,12 @@
-#!/usr/bin/python3
+# !/usr/bin/python3
+"""
+100-my-int.py
+"""
+
+
+def add_attribute(obj, name, value):
+    """ Adds a attribute to a given object """
+    if hasattr(obj, '__dict__') is False:
+        raise TypeError("can't add new attribute")
+    else:
+        setattr(obj, name, value)
