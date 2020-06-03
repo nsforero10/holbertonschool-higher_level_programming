f45e0fcbe2bd5dfa3ee208eaf01ac784404c8223
Nicolas Forero <ns.forero10@gmail.com>
2020-06-02 23:54:50 -0500

    [Fix] Fixing typos task cases 0x0A

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -6,13 +6,13 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """Square class that inherits from rectangle"""
+    """ Square class that inherits from rectangle """
     def __init__(self, size):
-        """ Constructs a square"""
+        """ Constructs a square """
         if self.integer_validator("size", size):
             self.__size = size
         super().__init__(size, size)
 
     def __str__(self):
-        """str representation"""
+        """ str representation """
         return "[Square] {:d}/{:d}".format(self.__size, self.__size)
