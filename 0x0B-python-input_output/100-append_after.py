991081b35a29862ddc9c58e7c084465e3d562924
Nicolas Forero <ns.forero10@gmail.com>
2020-06-04 00:14:59 -0500

    [Feature] Adding tasks 2-14 tasks 0x0b

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""
+100-append_after.py
+"""
+
+def append_after(filename="", search_string="", new_string=""):
+    """
+    appends the new string after below the search string
+    """
+    buffer = ''
+    with open(filename, "r+") as f:
+        for ln in f:
+            buffer += ln
+            if search_string in ln:
+                buffer += new_string
+    with open(filename, 'w') as fw:
+        fw.write(buffer)
