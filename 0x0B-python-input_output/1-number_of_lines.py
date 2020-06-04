1cdf37295fa7b0c093b4be708388e364d337189a
Nicolas Forero <ns.forero10@gmail.com>
2020-06-03 23:32:40 -0500

    [Feature] Adding tasks 0-1 cases 0x0b

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -7,4 +7,7 @@ number_of_lines
 def number_of_lines(filename=""):
     """Prints the number of lines in the file"""
     with open(filename, encoding='utf8') as f:
-        return len(filename)
+        lines = 0
+        for ln in f:
+            lines += 1
+        return lines
