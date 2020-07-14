71361fe7eb61ca04db1723b127334795ad85c73f
Nicolas <ns.forero10@gmail.com>
2020-07-13 23:10:44 -0500

    [Fix] Adding advanced tasks

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,5 +1,5 @@
 -- Shows higher temperatures that a state has had
 -- Shows higher temperatures that a state has had
 SELECT state, MAX(value) as max_temp from temperatures 
-    ORDER BY state;
-    GROUP BY state 
+    ORDER BY state
+    GROUP BY state; 
