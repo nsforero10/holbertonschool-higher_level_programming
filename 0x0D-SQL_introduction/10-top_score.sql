e1602c11d4d5956e7066e148a359cc3bb3843719
Nicolas <ns.forero10@gmail.com>
2020-07-13 22:33:50 -0500

    [Fix] small fixes

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,3 +1,3 @@
--- shows score and name clumns from second_table
--- shows score and name clumns from second_table
+-- Shows score and name clumns from second_table
+-- Shows score and name clumns from second_table
 SELECT score, name FROM second_table ORDER BY score DESC;
