6d45f99205c4c69609619b981999db5afc9cde84
Nicolas <ns.forero10@gmail.com>
2020-07-14 23:57:58 -0500

    [feature] adding whole tasks

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,3 @@
+-- Creates a user if doesn't exist
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'uder_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,9 @@
+-- creates data base and its data talbe filled
+SELECT id,
+    name
+FROM cities
+WHERE state_id = (
+        SELECT id
+        FROM states
+        WHERE name = "California"
+    );
