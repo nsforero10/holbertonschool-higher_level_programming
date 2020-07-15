75514d24365f8b6ab2aeec2166779699e69876ce
Nicolas <ns.forero10@gmail.com>
2020-07-15 00:11:33 -0500

    [feature] adding whole tasks

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,8 +1,9 @@
 -- list shows 
 -- list shows 
-SELECT tv_shows.title,
+SELECT tv_genres.name,
     SUM(tv_show_ratings.rate) AS rating
-FROM tv_shows
-    INNER JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id
-GROUP BY tv_shows.title
+FROM tv_genres
+    INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+    INNER JOIN tv_show_ratings ON tv_show_genres.show_id = tv_show_ratings.show_id
+GROUP BY tv_genres.name
 ORDER BY rating DESC;
