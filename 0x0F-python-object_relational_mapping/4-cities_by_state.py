6d297731b021c43e74fa532b9347d3f287a604b3
Nicolas Forero <ns.forero10@gmail.com>
2020-08-09 23:32:20 -0500

    fixing query task 4

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -12,8 +12,9 @@ if __name__ == "__main__":
 
     cursor = db.cursor()
     cursor.execute(
-        'SELECT cities.id, cities.name, states.name FROM cities JOIN states ON\
-        cities.state_id = states.id;')
+        'SELECT cities.id, cities.name, states.name FROM cities\
+                INNER JOIN states ON cities.state_id = states.id\
+                ORDER BY cities.id')
 
     cities = cursor.fetchall()
 
