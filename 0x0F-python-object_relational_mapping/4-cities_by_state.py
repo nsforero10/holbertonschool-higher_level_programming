f351c2164be55c26899247321b6ccf9cfa71b5a2
Nicolas Forero <ns.forero10@gmail.com>
2020-08-09 23:27:49 -0500

    fix wrong file

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -14,19 +14,13 @@ if __name__ == "__main__":
 
     cursor = db.cursor()
     cursor.execute(
-        'SELECT cities.name FROM cities\
-        INNER JOIN states ON cities.state_id = states.id\
-        WHERE states.name = %s \
-        ORDER BY cities.id ASC', (sys.argv[4], ))
+        'SELECT cities.id, cities.name, states.name FROM cities JOIN states ON\
+        cities.state_id = states.id;', (sys.argv[4], ))
 
     cities = cursor.fetchall()
 
-    idx = 0
     for city in cities:
-        if idx != 0:
-            print(", ", end="")
-        print("%s" % city, end="")
-        idx += 1
+        print(city)
     print("")
 
     cursor.close()
