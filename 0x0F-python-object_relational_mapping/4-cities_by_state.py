e46feb5235105963c89db10f7505c792b2e16de7
Nicolas Forero <ns.forero10@gmail.com>
2020-08-09 23:25:12 -0500

    [fix] 4 0x0f

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -5,21 +5,29 @@ in the states table of hbtn_0e_0_usa where name matches the argument'''
 if __name__ == "__main__":
     from sys import argv
     import MySQLdb
-    db = MySQLdb.connect(user=argv[1],
-                         passwd=argv[2],
-                         db=argv[3])
-    cr = db.cursor()
-    cr.execute("SELECT cities.name FROM cities\
-                INNER JOIN states ON cities.state_id = states.id\
-                WHERE states.name = %s\
-                ORDER BY cities.id", (argv[4], ))
-    cities = cr.fetchall()
-    first = 0
+    db = MySQLdb.connect(
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        port=3306,
+        host='localhost')
+
+    cursor = db.cursor()
+    cursor.execute(
+        'SELECT cities.name FROM cities\
+        INNER JOIN states ON cities.state_id = states.id\
+        WHERE states.name = %s \
+        ORDER BY cities.id ASC', (sys.argv[4], ))
+
+    cities = cursor.fetchall()
+
+    idx = 0
     for city in cities:
-        if first != 0:
+        if idx != 0:
             print(", ", end="")
         print("%s" % city, end="")
-        first += 1
+        idx += 1
     print("")
-    cr.close()
+
+    cursor.close()
     db.close()
