55e9ae7b1ebe26ae9db94a972dd8ae320b347c32
Nicolas Forero <ns.forero10@gmail.com>
2020-08-09 23:33:13 -0500

    [fix] refactor task 4

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -5,21 +5,15 @@ in the states table of hbtn_0e_0_usa where name matches the argument'''
 if __name__ == "__main__":
     from sys import argv
     import MySQLdb
-    db = MySQLdb.connect(
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3])
-
-    cursor = db.cursor()
-    cursor.execute(
-        'SELECT cities.id, cities.name, states.name FROM cities\
+    db = MySQLdb.connect(user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
+    cr = db.cursor()
+    cr.execute("SELECT cities.id, cities.name, states.name FROM cities\
                 INNER JOIN states ON cities.state_id = states.id\
-                ORDER BY cities.id')
-
-    cities = cursor.fetchall()
-
+                ORDER BY cities.id")
+    cities = cr.fetchall()
     for city in cities:
         print(city)
-
-    cursor.close()
+    cr.close()
     db.close()
