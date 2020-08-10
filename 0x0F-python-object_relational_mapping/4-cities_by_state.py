b560bec23007c61884581574c652f68e908eccfa
Nicolas Forero <ns.forero10@gmail.com>
2020-08-09 23:21:45 -0500

    [Fix] Fixing typo task 4 0x0F

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -9,16 +9,17 @@ if __name__ == "__main__":
                          passwd=argv[2],
                          db=argv[3])
     cr = db.cursor()
-    cr.execute('SELECT cities.id, cities.name, states.name FROM cities\
-        JOIN states ON cities.state_id = states.id', (argv[4],))
-    states = cr.fetchall()
-
-    i = 0
-    for state in states:
-        if i != 0:
+    cr.execute("SELECT cities.name FROM cities\
+                INNER JOIN states ON cities.state_id = states.id\
+                WHERE states.name = %s\
+                ORDER BY cities.id", (argv[4], ))
+    cities = cr.fetchall()
+    first = 0
+    for city in cities:
+        if first != 0:
             print(", ", end="")
-        print("%s" % state, end="")
-        i += 1
-    print('')
+        print("%s" % city, end="")
+        first += 1
+    print("")
     cr.close()
     db.close()
