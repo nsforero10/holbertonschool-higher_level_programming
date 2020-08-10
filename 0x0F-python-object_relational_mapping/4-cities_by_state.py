6990324178dacea8ef9d8562219ea556ab364c02
Nicolas Forero <ns.forero10@gmail.com>
2020-08-09 23:29:18 -0500

    fix 2 4

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     cursor = db.cursor()
     cursor.execute(
         'SELECT cities.id, cities.name, states.name FROM cities JOIN states ON\
-        cities.state_id = states.id;', (sys.argv[4], ))
+        cities.state_id = states.id;')
 
     cities = cursor.fetchall()
 
