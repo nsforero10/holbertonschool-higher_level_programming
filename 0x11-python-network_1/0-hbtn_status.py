eae1104f58448015f49a187d396cf8b7c1eaebe3
Nicolas Forero <ns.forero10@gmail.com>
2020-08-24 22:17:23 -0500

    Update: adding 0 task 0x11

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""fetches https://intranet.hbtn.io/status"""
+'''fetches https://intranet.hbtn.io/statu'''
 if __name__ == '__main__':
     import urllib.request as request
-    with request.urlopen('http://intranet.hbtn.io/status') as response:
-        html = response.read()
+    with request.urlopen('http://intranet.hbtn.io/status') as res:
+        html = res.read()
         print('Body response:')
         print('\t- type: {}'.format(type(html)))
         print('\t- content: {}'.format(html))
