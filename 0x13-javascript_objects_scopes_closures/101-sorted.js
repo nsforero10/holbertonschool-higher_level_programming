bea5a6daeac90501bead1bb18d1377aafba8ef54
Nicolas Forero <ns.forero@gmail.com>
2020-09-21 18:04:11 -0500

    Update: adding hole tasks 0x13

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -3,10 +3,10 @@ const dict = require('./101-data').dict;
 const newDict = {};
 
 for (const key in dict) {
-	if (typeof (newDict[dict[key]]) === 'undefined') {
-		newDict[dict[key]] = [];
-	}
-	newDict[dict[key]].push(key);
+  if (typeof (newDict[dict[key]]) === 'undefined') {
+    newDict[dict[key]] = [];
+  }
+  newDict[dict[key]].push(key);
 }
 
 console.log(newDict);
