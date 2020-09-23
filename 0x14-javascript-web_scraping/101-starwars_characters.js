f5148aceda4e58add8076c106dcb2f575cee6855
Nicolas Forero <ns.forero@gmail.com>
2020-09-23 01:23:30 -0500

    Fix: fixing spaces instead of tabs tasks 0x14

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -3,43 +3,43 @@ const request = require('request');
 const { argv, exit } = require('process');
 
 if (argv.length !== 3) {
-	exit(0);
+  exit(0);
 }
 
 const options = {
-	url: `https://swapi-api.hbtn.io/api/films/${argv[2]}/`,
-	method: 'GET',
-	headers: {
-		'Accept-Charset': 'utf-8'
-	}
+  url: `https://swapi-api.hbtn.io/api/films/${argv[2]}/`,
+  method: 'GET',
+  headers: {
+    'Accept-Charset': 'utf-8'
+  }
 };
 
 function requestStart (optionsUrl, saveValue) {
-	request(optionsUrl, function (err, res, body) {
-		if (err) throw err;
+  request(optionsUrl, function (err, res, body) {
+    if (err) throw err;
 
-		const characters = JSON.parse(body).characters;
-		const result = {};
+    const characters = JSON.parse(body).characters;
+    const result = {};
 
-		characters.forEach(element => {
-			request(element, function (err, res, _body) {
-				if (err) throw err;
+    characters.forEach(element => {
+      request(element, function (err, res, _body) {
+        if (err) throw err;
 
-				const name = JSON.parse(_body).name;
+        const name = JSON.parse(_body).name;
 
-				result[element.split('/')[5]] = name;
-				saveValue(result, characters.length);
-			});
-		});
-	});
+        result[element.split('/')[5]] = name;
+        saveValue(result, characters.length);
+      });
+    });
+  });
 }
 
 let i = 0;
 requestStart(options, function (dict, length) {
-	i++;
-	if (i === length) {
-		for (const name in dict) {
-			console.log(dict[name]);
-		}
-	}
+  i++;
+  if (i === length) {
+    for (const name in dict) {
+      console.log(dict[name]);
+    }
+  }
 });
